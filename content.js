9da013823c4275f415c225c43b5aef8889e73f8b
cm3cm <morancaleb3@gmail.com>
2023-06-25 12:18:43 -0400

    extract cardData

---
+++ content.js
@@ -0,0 +1,48 @@
+const cardData = [
+  {
+    title: "Software Engineering Intern",
+    company: "BTS Asset Management",
+    date: "June 2023 — Present",
+    description: `As an intern, I researched existing workflow inefficiencies,
+                  and boosted employee productivity by automating
+                  numerous processes. 
+                  Primarily working in Python, I utilized Pandas, Numpy, 
+                  and Matplotlib to save the firm hundreds of hours of 
+                  work each quarter. `,
+  },
+  {
+    title: "Software Design Intern",
+    company: "Develop for Good",
+    date: "May 2023 — Present",
+    description: `I joined a team of 8 designers 
+                  to re-design and upgrade the website for our non-profit client,
+                  <a href="https://tuskegeeairmen.org/">Tuskegee Airmen, Inc.</a>
+                  <br />
+                  Over the course of the internship, I planned deliverables with the client,
+                  performed user research, mocked and designed changes in Figma,
+                  and created a far more engaging, accessible, and successful site.`,
+  },
+  {
+    title: "Teaching Assistant: CSCI0170",
+    company: "Brown University",
+    date: "August 2022 — December 2022",
+    description: `I worked as a TA for Brown's introductory 
+                  computer sciences course, <a href="https://cs17-fall2022.github.io/">CS17</a>, which had 
+                  over 100 students enrolled. 
+                  During the semester, I wrote and revised course content to meet new standards.
+                  <br />
+                  Additionally, I led weekly CS labs for 25 students and graded their assignments and projects.
+                  I assisted students one-on-one during office hours and through questions submitted online.`,
+  },
+  {
+    title: "Software Development Intern",
+    company: "NOVA Web Development",
+    date: "March 2020 — September 2020",
+    description: `While still in high school, I joined the software development team and quickly developed skills in Django.
+                  I coded, tested, and published a graphics program (<a href="https://codeberg.org/GASP/GASP_Code">GASP</a>) for educators to use in their CS courses,
+                  all as the lead developer.
+                  I also participated in weekly meetings, frequently serving as a bug tester and QA.`,
+  },
+];
+
+export { cardData };
